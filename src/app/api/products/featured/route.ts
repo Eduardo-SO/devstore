c2ce07c984cb20c1c286e908d1bd3c62d7fa30a1
Eduardo S.O <eduardo.sool07@gmail.com>
2024-05-26 17:07:14 -0300

    remove: api folder

--- src/app/api/products/featured/route.ts
+++
@@ -1,9 +0,0 @@
-import data from '../data.json'
-
-export async function GET() {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-
-  const featuredProduct = data.products.filter((product) => product.featured)
-
-  return Response.json(featuredProduct)
-}
